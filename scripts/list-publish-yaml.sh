5a18155d13d721d455787630fb5d821b8c1dda2e
Richard Hess <eswat2@gmail.com>
2026-06-18 09:26:30 -0700

    refactor to sort DATA properly

--- scripts/list-publish-yaml.sh
+++ scripts/list-publish-yaml.sh
@@ -13,14 +13,20 @@ main() {
   log::section "publish.yml"
 
   cd $HOME/github
+
+  PRIORITY="eswat2 eswat42 proto-tailwindcss-clrs proto-tailwindcss-pxls"
   REPOS=$(ls */*/.github/workflows/publish.yml | cut -d "/" -f 2 | sed 's/loader-wc/loader/g')
 
-  for repo in ${REPOS}
+  echo ${PRIORITY} | tr ' ' '\n' > .tmp
+  echo ${REPOS} | tr ' ' '\n' >> .tmp
+  DATA=($(cat .tmp | awk '!x[$0]++'))
+
+  for repo in "${DATA[@]}"
   do
     log::key_value ${repo} ": " true
   done
 
-  jo -a ${REPOS} > ${CACHE}
+  jo -a ${DATA[*]} > ${CACHE}
 }
 
 main "$@"
