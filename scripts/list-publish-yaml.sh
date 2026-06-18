ac407206c1db7e863e61ab9f3f405258455ee227
Richard Hess <eswat2@gmail.com>
2026-06-18 10:50:14 -0700

    refactoring

--- scripts/list-publish-yaml.sh
+++ scripts/list-publish-yaml.sh
@@ -9,7 +9,29 @@ set -o pipefail
 source $HOME/bin/utils/log.sh
 CACHE=${HOME}/.pub-cache.json
 
+LIST=false
+
+parse_arguments() {
+  if [[ $# -gt 0 ]]; then
+    while [[ $# -gt 0 ]]; do
+      case $1 in
+      --list | 42)
+        log::output "--list | 42" green
+        LIST=true
+        shift
+        ;;
+      *)
+        log::output "-- INVALID option... [ $1 ]" red
+        exit
+        ;;
+      esac
+    done
+  fi
+}
+
 main() {
+  parse_arguments "$@"
+
   log::section "publish.yml"
 
   cd $HOME/github
@@ -21,10 +43,15 @@ main() {
   echo ${REPOS} | tr ' ' '\n' >> .tmp
   DATA=($(cat .tmp | awk '!x[$0]++'))
 
-  for repo in "${DATA[@]}"
-  do
-    log::key_value ${repo} ": " true
-  done
+  if [ ${LIST} == true ]
+  then
+    for repo in "${DATA[@]}"
+    do
+      log::key_value ${repo} ": " true
+    done
+  else
+    log::output "cache -- ${CACHE}" blue    
+  fi
 
   jo -a ${DATA[*]} > ${CACHE}
 }
